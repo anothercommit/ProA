20e5c7bd94fd52347d1a8bff00cbc1b5daea2bcf
anothercommit <lack_of_names@proton.me>
2023-10-06 21:51:21 -0300

    🦎

--- csharp/pilas/Program.cs
+++ csharp/pilas/Program.cs
@@ -8,14 +8,15 @@
             Stack<int> stack = new([1,2,3,4,5]);
 
             foreach (int e in stack) 
-                Console.Write(e);
+                Console.Write($"{e}, ");
             
             Console.WriteLine();
 
             stack = metodos.invertirStack(stack);
 
             foreach (int e in stack) 
-                Console.Write(e);
+                Console.Write($"{e}, ");
+            Console.WriteLine();
         }
     }
 }
