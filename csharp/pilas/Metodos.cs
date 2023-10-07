4ed5301cc695aeedb53b88d2360a6f0a5f556c47
anothercommit <lack_of_names@proton.me>
2023-10-07 11:49:39 -0300

    🦎

--- csharp/pilas/Metodos.cs
+++ csharp/pilas/Metodos.cs
@@ -1,11 +1,37 @@
-namespace Pilas {
-    class Metodos {
-        public Stack<int> invertirStack(Stack<int> stack){
+namespace Pilas
+{
+    class Metodos
+    {
+        public Stack<int> InvertirStack(Stack<int> stack)
+        {
             Stack<int> result = new();
-            for (int i =0; i < stack.Count; i++)
+            while (stack.Count > 0)
                 result.Push(stack.Pop());
-            
+
             return result;
+        }
+
+        public bool Balanceado(string str)
+        {
+            Stack<char> stack = new();
+
+            foreach (char e in str)
+            {
+                if (e == '(') stack.Push(e);
+                else if (e == ')')
+                {
+                    if (stack.TryPop(out char result) == false) return false;
+                }
+                else return false;
+            }
+
+            return stack.Count == 0 ? true : false;
+        }
+
+        public void MostrarStack(Stack<int> stack)
+        {
+            foreach (int e in stack)
+                Console.Write(e);
         }
     }
 }
--- csharp/pilas/Program.cs
+++ csharp/pilas/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Pilas
+{
+    class Program
+    {
+        static void Main()
+        {
+            Metodos meth = new(); // clase con las actividades
+            Stack<int> nums = new([0, 1, 2, 3]);
+            string str = "(())";
+
+            Console.Write($"Invertir:\t");
+            meth.MostrarStack(nums);
+            Console.Write(" -> ");
+            meth.MostrarStack(meth.InvertirStack(nums));
+
+            Console.WriteLine($"\nBalanceado:\t{str} -> {meth.Balanceado(str)}");
+        }
+    }
+}
