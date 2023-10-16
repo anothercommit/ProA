ef28bb2bc93e102e629b7a1e8c5b05d176c8f7dd
anothercommit <lack_of_names@proton.me>
2023-10-16 10:26:00 -0300

    🦎

--- csharp/guia3ro/obj/Debug/net8.0/guia3ro.AssemblyInfo.cs
+++ csharp/guia3ro/obj/Debug/net8.0/guia3ro.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("guia3ro")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50c46062821636937237a5cf16a30d400ff79ebb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("guia3ro")]
 [assembly: System.Reflection.AssemblyTitleAttribute("guia3ro")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
