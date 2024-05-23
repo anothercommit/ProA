ba3884f1f1b0938e7280439c3933162fc97febc4
anothercommit <lack_of_names@proton.me>
2024-05-23 17:56:15 -0300

    🦎

--- python/wtia_api/main.py
+++ python/wtia_api/main.py
@@ -6,9 +6,7 @@ api_url = "https://api.wheretheiss.at/v1/satellites/25544"
 res = requests.get(api_url)
 iss_data = json.loads(res.content)
 
-final_url = (
-    f"https://www.google.com/maps/place/{
+final_url = f"https://www.google.com/maps/place/{
         iss_data['latitude']},{iss_data['longitude']}"
-)
 
 webbrowser.open_new(final_url)
