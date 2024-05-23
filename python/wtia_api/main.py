fcc6ec592aff1e0aaa4002a3ce7826810043a1cc
Tu nombre <you@example.com>
2024-05-22 21:54:40 -0300

    🦎

--- python/wtia_api/main.py
+++ python/wtia_api/main.py
@@ -1,10 +1,14 @@
 import requests
 import json
+import webbrowser
 
-url = "https://api.wheretheiss.at/v1/satellites/25544"
-res = requests.get(url)
+api_url = "https://api.wheretheiss.at/v1/satellites/25544"
+res = requests.get(api_url)
 iss_data = json.loads(res.content)
 
-location = (
-    f"https://www.google.com/maps/place/{iss_data['latitude']},{iss_data['longitude']}"
+final_url = (
+    f"https://www.google.com/maps/place/{
+        iss_data['latitude']},{iss_data['longitude']}"
 )
+
+webbrowser.open_new(final_url)
