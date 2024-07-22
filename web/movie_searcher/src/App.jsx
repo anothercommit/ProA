fb57dd5db62ac8cfc8a3f3d5e7ee0c4e89ba1532
Your Name <you@example.com>
2024-07-22 16:51:50 -0300

    Mon Jul 22 04:51:50 PM -03 2024

--- web/movie_searcher/src/App.jsx
+++ web/movie_searcher/src/App.jsx
@@ -1,41 +1,46 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+
+// API key: 1dcbcd87
 
 function App() {
     const [searchValue, setSearchValue] = useState('');
-    const [movie, setMovie] = useState('');
+    const [movies, setMovies] = useState('');
+    let movie = ''
 
     const handleChange = (event) => {
-        setSearchValue(event.target.value);
+        movie = event.target.value
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setSearchValue(movie)
     };
 
     useEffect(() => {
-        // fetch(``)
-        //     .then(res => res.json())
-        //     .then(json => setProducts(json))
-        console.log(movie)
-    }, [movie]);
+        console.log(searchValue)
+        fetch(`http://www.omdbapi.com/?apikey=1dcbcd87&t=${searchValue}`)
+            .then(res => res.json())
+            .then(json => setMovies([...movies, json]))
+            .then(console.log(movies))
+    }, [searchValue]);
 
     return (
         <>
             <label htmlFor="movie-search">Busque el nombre de una peli</label>
             <br />
             <br />
-            <form action="GET">
+            <form action="">
                 <input
                     type="search"
-                    value={searchValue}
+                    // value={searchValue}
                     onChange={handleChange}
                     placeholder="Search..."
                 />
-                <button
-                    type='submit'
-                    onClick={() => setMovie(searchValue)}
-                >
-                    Buscar
-                </button>
+                <button type='submit' onClick={handleSubmit}>Buscar</button>
+                <p>Search Value: {searchValue}</p>
+                <p>Movie: {movie}</p>
             </form>
-            <p>Search Value: {searchValue}</p>
             <br />
             <br />
         </>
