843dfd21b644f4aa21329f32b3b35dedceb3a6d8
Tu nombre <you@example.com>
2024-08-19 07:36:37 -0300

    lunes, 19 de agosto de 2024, 07:36:37 -03

--- Recursos utiles/VITE_COPYPASTE/src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <>
-      Hola Mundo
-    </>
-  )
-}
-
-export default App
---
+++ Recursos utiles/VITE_TEMPLATE/src/App.jsx
@@ -0,0 +1,17 @@
+"use strict";
+
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+
+function App() {
+
+  const baseUrl = "http://localhost:3000"
+
+  return (
+    <>
+    </>
+  )
+}
+
+export default App
