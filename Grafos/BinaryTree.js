9dd5b69b21cc1dd128917ca2068d95efd19b20d3
Your Name <you@example.com>
2024-10-29 13:17:35 -0300

    Tue Oct 29 01:17:35 PM -03 2024

---
+++ Grafos/BinaryTree.js
@@ -0,0 +1,12 @@
+export default class BinaryTree {
+  constructor() {
+    this.root = null;
+  }
+
+  insert(value) {
+    const newNode = new Node(value);
+  }
+
+  _insertRecursively(currentNode, newNode) {
+  }
+}
